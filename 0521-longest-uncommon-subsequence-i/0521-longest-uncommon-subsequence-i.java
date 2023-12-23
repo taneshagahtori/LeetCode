d0fb552268ae35734e69713728a53f38ddbc9663
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2023-12-24 02:33:36 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (7.19%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)){
+            return -1;
+        }
+        else if(a.length()>b.length()){
+            return a.length();
+        }
+        else {
+            return b.length();
+        }
+    }
+}
