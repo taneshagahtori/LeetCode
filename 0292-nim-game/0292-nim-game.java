8b7569691c7b70a77388148a092430e5aeb98109
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2023-12-27 23:26:03 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (6.40%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n%4==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
