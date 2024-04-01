0883057964b8e86fb3893c6300e695f0f73c167b
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-03-31 23:37:33 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (18.81%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int lastIndex= s.lastIndexOf(' ')+1;
+        return s.length()-lastIndex;
+        
+    }
+}
