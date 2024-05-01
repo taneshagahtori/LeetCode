a27f86883733c22edd68099e7119f6515551b8ad
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-05-01 21:59:53 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (91.13%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int idx=word.indexOf(ch);
+        if(idx==-1) return word;
+        StringBuilder ans=new StringBuilder(word.substring(0,idx+1));
+        ans.reverse();
+        ans.append(word.substring(idx+1));
+        return ans.toString();
+    }
+}
