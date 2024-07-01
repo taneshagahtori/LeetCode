faca128bb7a54edad8feaeecd688d1828c348cc5
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-07-01 17:24:03 +0530

    Time: 0 ms (100.00%), Space: 49 MB (39.79%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,11 +1,15 @@
 class Solution {
     public void reverseString(char[] s) {
-        StringBuilder sb= new StringBuilder();
-        sb.append(s);
-        sb.reverse();
-        
-        for(int i=0; i<s.length; i++){
-            s[i]=sb.charAt(i);
+        int left=0;
+        int right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
         }
     }
 }
+
+
