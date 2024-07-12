24f24d13161721e230389c0bea439438577e8316
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-07-12 23:23:30 +0530

    Time: 2 ms (64.39%), Space: 40.7 MB (33.17%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int totalNQueens(int n) {
+        boolean board[][]=new boolean[n][n];
+        return queens(board,0);   
+    }
+    
+    int queens(boolean[][] board, int row) {
+        if (row == board.length) return 1;
+        int count = 0;
+        // place the queen and check row and column
+        for (int col = 0; col < board.length; col++) 
+            // place the queen if it is safe
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                board[row][col] = false;
+            }
+        return count;
+    }
+    
+      boolean isSafe(boolean[][] board, int row, int col) {
+        // checking vertical
+        for (int i = 0; i < row; i++)
+            if (board[i][col]) return false;
+        // checking left diagonal
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++)
+            if (board[row - i][col - i]) return false;
+        // checking right diagonal
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++)
+            if (board[row - i][col + i]) return false;
+        // if none of the checks are false, the position is safe
+        return true;
+    }
+}
