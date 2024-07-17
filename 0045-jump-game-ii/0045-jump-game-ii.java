28801489d2d6e7ddfd81fc711fd7d4bdd774bd3d
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-07-17 23:45:59 +0530

    Time: 2 ms (62.52%), Space: 45.2 MB (29.60%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int jump(int[] nums) {
+        int l=0, r=0;
+        int jump=0;
+        if(nums.length==1){
+            return 0;
+        }
+        for(int i=0; i<nums.length-1; i++){
+            r=Math.max(r, nums[i]+i);
+            if(l==i){
+                l=r;
+                jump++;
+            }
+        }
+        return jump;
+    }
+}
+
+
+
+
+
+// class Solution {
+//     public int jump(int[] arr) {
+//         if(arr.length==1) return 0;
+//         int n = arr.length;
+//         int count =0;
+//         int max = 0;
+//         int curr = 0;
+//         for(int i=0; i<n-1;i++){
+//             max = Math.max(max, arr[i]+i);
+//             if(curr==i){
+//                 curr = max;
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
